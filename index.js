adf052129d7a99f40f148190d1e12423364b8a26
yashablack <iamyashablack@gmail.com>
2023-07-28 01:46:59 +0300

    fix: updated Cardano logos

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
 const path = require("path");
 
-fs.readdirSync(path.join(__dirname, "./node_modules/cardano-token-registry/mappings")).forEach((fileName, index) => {
-    const file = fs.readFileSync(path.join(__dirname, `./node_modules/cardano-token-registry/mappings/${fileName}`));
+fs.readdirSync(path.join(__dirname, "./cardano-token-registry/mappings")).forEach((fileName) => {
+    const file = fs.readFileSync(path.join(__dirname, `./cardano-token-registry/mappings/${fileName}`));
     const json = JSON.parse(file.toString())
 
     if (json.logo && json.logo.value && json.subject) {
