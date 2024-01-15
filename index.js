557e3c9c5312f971c5d7018e48a9d2d97b69ae19
rsalakhov <r.salahov@spectrumlabs.fi>
2024-01-15 18:18:17 +0300

    remove cardano logos

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-const fs = require("fs");
-const path = require("path");
-
-fs.readdirSync(path.join(__dirname, "./cardano-token-registry/mappings")).forEach((fileName) => {
-    const file = fs.readFileSync(path.join(__dirname, `./cardano-token-registry/mappings/${fileName}`));
-    const json = JSON.parse(file.toString())
-
-    if (json.logo && json.logo.value && json.subject) {
-        const byteString = json.logo.value
-
-        const buffer = Buffer.from(byteString,'base64');
-
-        const filePath = `./logos/cardano/${json.subject}.png`;
-
-        fs.writeFileSync(filePath, buffer);
-    }
-});
+console.log('start')
